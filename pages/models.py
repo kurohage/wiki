a1f918000de57c1c39d97a6d2128e27bb8d5c85e
kurohage <517326+kurohage@users.noreply.github.com>
2019-10-02 20:13:32 +0300

    completed

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Page(models.Model):
+	title = models.CharField(max_length=25)
+	content = models.TextField()
+	last_update = models.DateTimeField(auto_now=True)
+
+	def get_absolute_url(self):
+		from django.urls import reverse
+		return reverse('page-detail', args=[str(self.id)])
+
+	def __str__(self):
+		return self.title
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Page
 
 # Create your views here.
+def page_list(request):
+    context = {
+        "pages": Page.objects.all()
+    }
+    return render(request, 'list.html', context)
+
+
+def page_detail(request, page_id):
+    context = {
+        "page": Page.objects.get(id=page_id),
+    }
+    return render(request, 'detail.html', context)
+
+def __str__(self):
+	return self.title
+
+def get_absolute_url(self):
+	from django.urls import reverse
+	return reverse('pages.views.detail', args=[str(self.id)])
