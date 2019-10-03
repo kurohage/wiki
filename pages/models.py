e4a77e753947f93a378b0417f537351ff212347d
kurohage <517326+kurohage@users.noreply.github.com>
2019-10-03 17:31:59 +0300

    updated models to use kwargs instead of args

--- pages/models.py
+++ pages/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Page(models.Model):
@@ -7,8 +8,9 @@ class Page(models.Model):
 	last_update = models.DateTimeField(auto_now=True)
 
 	def get_absolute_url(self):
-		from django.urls import reverse
-		return reverse('page-detail', args=[str(self.id)])
+		#from django.urls import reverse
+		#return reverse('page-detail', args=[str(self.id)])
+		return reverse('page-detail', kwargs={"page_id": self.id})
 
 	def __str__(self):
 		return self.title
